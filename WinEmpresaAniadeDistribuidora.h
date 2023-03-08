a9a8878f78e33e7f7e44027b68de0d443636c3b4
MeliGrasso <126627109+MeliGrasso@users.noreply.github.com>
2023-03-08 07:12:33 -0300

    Add files via upload

---
+++ WinEmpresaAniadeDistribuidora.h
@@ -0,0 +1,20 @@
+#ifndef WINEMPRESAANIADEDISTRIBUIDORA_H
+#define WINEMPRESAANIADEDISTRIBUIDORA_H
+#include "wxfb_project.h"
+#include "WinEmpresaAniadeDistribuidora.h"
+#include "Ventas.h"
+
+class WinEmpresaAniadeDistribuidora : public Empresa_AddDistrib {
+	
+private:
+	Ventas *m_System;
+	int m_indice_empresa;
+protected:
+	void OnClickGuardar( wxCommandEvent& event )  override;
+	void OnClickVolver( wxCommandEvent& event )  override;
+public:
+	WinEmpresaAniadeDistribuidora(wxWindow *parent, Ventas *system, int Empresa_a_editar);
+};
+
+#endif
+
